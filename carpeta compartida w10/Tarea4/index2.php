bab0c02d81922fdf3e1ff29e2b5b6a50099b4c72
2DAW13 <2DAW13@DOMDPTOINFJMPP.INF>
2022-09-20 18:24:52 +0200

    Septiembre

--- carpeta compartida w10/Tarea4/index2.php
+++ carpeta compartida w10/Tarea4/index2.php
@@ -7,48 +7,10 @@
      </head>
 <body>
 <div class="cuadro">
-    <h1>COLOR ELEGIDO</h1>
-    Nombre
-        <p>
-            <?php 
-            $nombre = $_POST["nombre"]; 
-            echo $nombre;
+            <?php  
+            $color = $_POST["aaa"]; 
+            echo "<body style=\"background-color: $color\">";
             ?>
-        </p><br>
-        <hr>
-    Email
-        <p>
-            <?php 
-            $email = $_POST["email"]; 
-            echo $email;
-            ?>
-        </p><br>
-        <hr>
-    Edad
-        <p>
-            <?php 
-            $edad = $_POST["edad"]; 
-            echo $edad;
-            ?>
-        </p>
-        <hr>
-    Sueldo
-        <p>
-            <?php 
-            $sueldo = $_POST["sueldo"];
-            echo $sueldo;
-            ?>
-        </p><br>
-        <hr>
-    SueldoIPC
-        <p>
-            <?php 
-            $ipc= $_POST["ipc"];
-            $sueldo = $_POST["sueldo"];
-            echo $sueldo + $sueldo * $ipc / 100;
-            ?>
-        </p><br>
-        <hr>
         <a href="index.html" target="_self">Volver</a>
 </div>
 </body>
